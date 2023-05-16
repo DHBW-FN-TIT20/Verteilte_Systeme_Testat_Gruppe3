a147d77b832ab2a236984bdf17841ed51350ac3e
Nico Bayer <82388053+NicoB-Code@users.noreply.github.com>
2023-05-16 11:48:25 +0200

    Grundfunktion Server
    Funktionsprototypen für die erforderlichen Dienste aufgesetzt.

--- src/server.cpp
+++ src/server.cpp
@@ -12,8 +12,171 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
+#include <ctime>
 
-// Funktion zum Starten des Servers
+
+
+// ----------------------------------------------------
+// SECTION SUBSCRIBE TOPIC
+// ----------------------------------------------------
+enum class SubscribeResult {
+    SUCCESS,
+    INVALID_PARAMETERS,
+    INTERNAL_ERROR
+};
+
+SubscribeResult subscribeTopic(const std::string& topicName) {
+    // Überprüfe die Gültigkeit der Parameter
+    if (topicName.empty()) {
+        return SubscribeResult::INVALID_PARAMETERS;
+    }
+
+    // Hier erfolgt die Logik zur Registrierung des Subscribers auf das Topic
+    // Überprüfung, ob das Topic bereits existiert oder erstellt werden muss
+    // Registrierung des Subscribers auf das Topic
+
+    // Beispiel: Annahme, dass die Registrierung erfolgreich ist
+    return SubscribeResult::SUCCESS;
+}
+
+// ----------------------------------------------------
+// SECTION UNSUBSCRIBE TOPIC
+// ----------------------------------------------------
+std::string unsubscribeTopic(const std::string& name) {
+    // Implementiere hier die Logik zur Abmeldung des Subscribers vom Topic
+    // Überprüfe die Parameter, prüfe die Existenz des Topics und entferne die Registrierung
+    // Gib entsprechende Rückgabewerte basierend auf dem Ergebnis zurück
+
+    // Beispielcode:
+    if (name.empty()) {
+        return "Ungültige Parameter";
+    }
+
+    // Überprüfe, ob das Topic existiert und der Subscriber registriert ist
+    //bool topicExists = /* Prüfe, ob das Topic existiert */;
+    //bool registrationExists = /* Prüfe, ob der Subscriber registriert ist */;
+
+    //if (!topicExists || !registrationExists) {
+    //    return "Topic/Registrierung existiert nicht";
+    //}
+
+    // Führe die Abmeldung durch
+    // ...
+
+    return "Erfolgreich von Topic abgemeldet";
+}
+
+// ----------------------------------------------------
+// SECTION PUBLISH TOPIC
+// ----------------------------------------------------
+// Funktion zum Publizieren einer Nachricht auf dem Topic
+bool publishTopic(const std::string& topicName, const std::string& message) {
+    // Überprüfen, ob das Topic existiert
+    //if (!topicExists(topicName)) {
+    //    std::cout << "Topic existiert nicht" << std::endl;
+    //    return false;
+    //}
+
+    // Überprüfen der Parameter
+    if (topicName.empty() || message.empty()) {
+        std::cout << "Ungültige Parameter" << std::endl;
+        return false;
+    }
+
+    // Weitere interne Logik zur Aktualisierung des Topics mit der neuen Nachricht
+    // ...
+
+    std::cout << "Topic erfolgreich aktualisiert" << std::endl;
+    return true;
+}
+
+// ----------------------------------------------------
+// SECTION LIST TOPICS
+// ----------------------------------------------------
+// Function to retrieve the list of available topics
+std::vector<std::string> listTopics()
+{
+    std::vector<std::string> topics;
+
+    // Add sample topics to the list
+    topics.push_back("Topic 1");
+    topics.push_back("Topic 2");
+    topics.push_back("Topic 3");
+
+    return topics;
+}
+
+struct TopicStatus {
+    std::time_t lastUpdated;
+    std::vector<std::string> subscribers;
+};
+
+// ----------------------------------------------------
+// SECTION GET TOPIC STATUS
+// ----------------------------------------------------
+// Funktion, um den aktuellen Status eines Topics abzurufen
+TopicStatus getTopicStatus(const std::string& topicName) {
+    TopicStatus status;
+
+    // Überprüfen, ob das Topic existiert
+    std::vector<std::string> topics = listTopics(); // Annahme: Die Funktion listTopics() gibt eine Liste der verfügbaren Topics zurück
+
+    bool topicExists = false;
+    for (const auto& topic : topics) {
+        if (topic == topicName) {
+            topicExists = true;
+            break;
+        }
+    }
+
+    if (!topicExists) {
+        // Topic existiert nicht
+        std::cout << "Topic existiert nicht" << std::endl;
+        return status;
+    }
+
+    // Hier weiter mit der Implementierung, um den Status des Topics abzurufen
+    // status.lastUpdated = ... // Setze den Zeitstempel der Aktualisierung
+    // status.subscribers = ... // Setze die Liste der Subscriber
+
+    return status;
+}
+
+// Struktur, um Informationen zu einem Topic zu speichern
+struct Topic {
+    std::string name;
+    std::string message;
+    std::string timestamp;
+    std::vector<std::string> subscribers;
+};
+
+// Globale Map zum Speichern der Topics
+std::map<std::string, Topic> topics;
+
+// Funktion zum Übermitteln des Topic-Inhalts an alle Subscriber
+void updateTopic(const std::string& name, const std::string& message, const std::string& timestamp) {
+    // Überprüfen, ob das Topic existiert
+    auto it = topics.find(name);
+    if (it == topics.end()) {
+        std::cout << "Topic '" << name << "' existiert nicht" << std::endl;
+        return;
+    }
+
+    // Aktualisieren der Topic-Nachricht und des Zeitstempels
+    it->second.message = message;
+    it->second.timestamp = timestamp;
+
+    // Übermitteln der Nachricht an alle Subscriber
+    std::cout << "Update für Topic '" << name << "' wird an alle Subscriber gesendet" << std::endl;
+    for (const std::string& subscriber : it->second.subscribers) {
+        std::cout << "Nachricht an Subscriber '" << subscriber << "': " << message << std::endl;
+    }
+}
+
+// ----------------------------------------------------
+// SECTION START SERVER
+// ----------------------------------------------------
 void startServer() {
     // Windows-spezifische Initialisierung der Winsock-Bibliothek
     #ifdef _WIN32
