8a3a37f9e66ec337d5d80a8e9fe499dd0e594d8f
mp1621 <85688755+mp1621@users.noreply.github.com>
2023-06-06 07:59:24 +0200

    Added Server Communication Functions

--- src/client.cpp
+++ src/client.cpp
@@ -4,45 +4,49 @@
 #include <tclap/CmdLine.h>
 
 const int BUFFER_SIZE = 1024;
-const int PORT = 12345;
 
-int main(int argc, char** argv) {
-
-    TCLAP::CmdLine cmd("Command description message", ' ', "0.9");
-    TCLAP::SwitchArg listArg ("l", "list", "List all topics", false);
-    TCLAP::ValueArg<std::string> portArg("", "port", "Port of the server: ", false, "", "string");
-    TCLAP::ValueArg<std::string> topicArg("", "topic", "Topic to subscribe to", false, "", "string");
-    TCLAP::MultiArg<std::string> publishArg("p", "publish", "Publish a message", false, "string");
-
-
-    cmd.add(portArg);
-    cmd.add(topicArg);
-    cmd.add(publishArg);
-    cmd.add(listArg);
-    cmd.parse(argc, argv);
-
-    //use port for every connection
-    if (topicArg.isSet())
-    {
-        // subscribe to topic
-        // t: Topic1,Topic2 etc.
-    } else if (publishArg.isSet())
-    {
-        // publish message
-        // t: Topic1 m: Message
-    } else if (listArg.isSet())
-    {
-        // list all topics
-        // Message to server: list all
-    } else
-    {
-        std::cout << "No command specified" << std::endl;
+bool connectToServer(SOCKET clientSocket, std::string ip, std::uint16_t port)
+{
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = inet_addr(ip.c_str());
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Fehler beim Herstellen der Verbindung: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return false;
     }
-    exit(0);
-    
+    return true;
+}
 
+bool sendToServer(SOCKET clientSocket, std::string message)
+{
+    if (send(clientSocket, message.c_str(), strlen(message.c_str()), 0) == SOCKET_ERROR) {
+        std::cerr << "Fehler beim Senden der Daten: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return false;
+    }
+    return true;
+}
 
+bool receiveFromServer(SOCKET clientSocket, char* buffer)
+{
+    memset(buffer, 0, BUFFER_SIZE);
+    int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytesRead == SOCKET_ERROR) {
+        std::cerr << "Fehler beim Empfangen der Daten: " << WSAGetLastError() << std::endl;
+        return false;
+    } else {
+        std::cout << "Antwort vom Server: " << buffer << std::endl;
+    }
+    return true;
+}
 
+SOCKET createSocket()
+{
+    // Winsock initialisieren
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "Fehler beim Initialisieren von Winsock." << std::endl;
@@ -56,44 +60,89 @@ int main(int argc, char** argv) {
         WSACleanup();
         return 1;
     }
+    return clientSocket;
+}
 
-    // Server-Adresse und Port festlegen
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");  // IP-Adresse des Servers hier eintragen
-    serverAddress.sin_port = htons(PORT);
 
-    // Verbindung zum Server herstellen
-    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
-        std::cerr << "Fehler beim Herstellen der Verbindung: " << WSAGetLastError() << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
+int main(int argc, char** argv) {
 
-    // Daten senden
-    const char* message = "Hello, Server!";
-    if (send(clientSocket, message, strlen(message), 0) == SOCKET_ERROR) {
-        std::cerr << "Fehler beim Senden der Daten: " << WSAGetLastError() << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
+    TCLAP::CmdLine cmd("Publisher/Subscriber System", ' ', "1");
+    TCLAP::SwitchArg listArg ("", "list", "List all topics", false);
+    TCLAP::ValueArg<std::string> portArg("", "port", "Port of the server", false, "", "string");
+    TCLAP::MultiArg<std::string> topicArg("", "topic", "Topic to subscribe to", false, "string");
+    TCLAP::MultiArg<std::string> publishArg("", "publish", "Publish a message to a topic. Syntax: TOPIC:MESSAGE", false, "string");
 
-    // Antwort des Servers empfangen
-    char buffer[BUFFER_SIZE];
-    memset(buffer, 0, BUFFER_SIZE);
-    int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
-    if (bytesRead == SOCKET_ERROR) {
-        std::cerr << "Fehler beim Empfangen der Daten: " << WSAGetLastError() << std::endl;
+    cmd.add(portArg);
+    cmd.add(topicArg);
+    cmd.add(publishArg);
+    cmd.add(listArg);
+    cmd.parse(argc, argv);
+
+    std::uint16_t port = 12345;
+    std::string ip = "127.0.0.1";
+    //use port for every connection
+    if (portArg.isSet()) 
+    {
+        port = std::stoi(portArg.getValue());
     } else {
-        std::cout << "Antwort vom Server: " << buffer << std::endl;
+        std::cout << "No port specified. Using standard Port: " + std::to_string(port) << std::endl;
     }
 
-    // Socket schließen und Winsock aufräumen
-    closesocket(clientSocket);
-    WSACleanup();
+    SOCKET clientSocket = createSocket();
+    std::cout << "Connecting to server address: " + ip + ":" + std::to_string(port) << std::endl;
+    connectToServer(clientSocket, ip, port);
+
+    if (listArg.isSet())
+    {
+        // list all topics
+        // Message to server: list all
+        sendToServer(clientSocket, "$l");
+        char* buffer;
+        bool response = receiveFromServer(clientSocket, buffer);
+        if (response) {
+            std::cout << buffer << std::endl;
+        } else {
+            std::cout << "No response from server" << std::endl;
+        }
+    } else if (topicArg.isSet())
+    {
+        // subscribe to topic
+        // $t Topic1;Topic2 etc.
+        std::vector<std::string> topics = topicArg.getValue();
+        std::string message = "$t ";
+        for (std::string i : topics) {
+            message += i + ";";
+        }
+        message.pop_back();
+        sendToServer(clientSocket, message);
+        char* buffer;
+        bool response = receiveFromServer(clientSocket, buffer);
+        if (!response) {
+            std::cout << "No response from server" << std::endl;
+        } else {
+            //Ausgabe der Texte
+        }
+    } else if (publishArg.isSet())
+    {
+        // publish message
+        // t$Topic1 m$ Message
+        // $p TOPIC$MESSAGE;TOPIC$MESSAGE etc.;
+        std::string message = "$p ";
+        for (std::string i : publishArg.getValue()) {
+            message += i + ";";
+        }
+        sendToServer(clientSocket, message);
+        char* buffer;
+        bool response = receiveFromServer(clientSocket, buffer);
+        if (!response) {
+            std::cout << "No response from server" << std::endl;
+        } else {
+            std::cout << buffer << std::endl;
+        }
+    } else
+    {
+        std::cout << "No command specified" << std::endl;
+    }
 
     return 0;
 }
-
